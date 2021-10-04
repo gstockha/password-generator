7e06a806d43d09b119a1f948d989b4b910d1ccf1
gstockha <54012873+gstockha@users.noreply.github.com>
2021-10-04 16:43:28 -0500

    removed parentheses to fix button error

--- script.js
+++ script.js
@@ -69,4 +69,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
